d7d919cd23ca9b5a87c23e6400809d2d78b330a2
Hasin Hayder <ironhide@leevio.com>
2015-07-20 00:59:52 +0600

    smart summation

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var numbers = [1,2,3,4,5];
+var numbers = [1,2,3,4,5,6];
 
 function sum (n){
 	var summation = 0; 
 	
 	//do the actual sum
-
+	summation = n.reduce(function (o,n) { return o + n ; });
 	return summation;
 }
 
